5af5754f472d62cd657f1ac49ef8b4c61bb9e476
tax1driver <fajerowsky.yt@gmail.com>
2024-07-09 18:29:31 +0200

    feat: add metadata to errors

--- src/errors/P24Error.ts
+++ src/errors/P24Error.ts
@@ -31,8 +31,15 @@
  * @extends {Error}
  */
 export class P24Error extends Error {
+    public error: string;
+    public code: Number;
+
     constructor(error: string, code: Number, extra?: unknown) {
         super(`error = ${error}, code = ${code}, extra = ${extra}`); // 'Error' breaks prototype chain here
+
+        this.error = error;
+        this.code = code;
+        
         Object.setPrototypeOf(this, new.target.prototype);
     }
 }
